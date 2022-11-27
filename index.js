847e77c42bfa82c05e5c45f452f72a39d35dadc4
WaRtrO <morotmaxence@live.fr>
2022-11-27 23:34:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+
+// const coucouModule = require('./coucouModule.node');
+// console.log(coucouModule.coucouFonc('Maxence'));
+
+const helloWorldModule = require('./helloModule.node');
+console.log(helloWorldModule.helloWorld());
+
+// const additionModule = require('./additionModule.node');
+// console.log(additionModule.additionCalc(564984784894784787478748777878777877878797778,878787787778777));
+
+// const multiplicationModule = require('./multiplicationModule.node');
+// console.log(multiplicationModule.multiplicationCalc(2,4));
+
+
+
+
+// const perlinModule = require('./perlinModule.node');
+// console.time('test');
+// console.log(perlinModule.generate(59.595,69.757));
+// console.timeEnd('test');
