6fc7630325cdd37469111a363bb6317c6311e731
WaRtrO <morotmaxence@live.fr>
2022-11-27 23:36:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-/* eslint-disable max-len */
 
-// const coucouModule = require('./coucouModule.node');
-// console.log(coucouModule.coucouFonc('Maxence'));
+const coucouModule = require('./coucouModule.node');
+console.log('coucou module',coucouModule.coucouFonc('WaRtrO'));
 
 const helloWorldModule = require('./helloModule.node');
-console.log(helloWorldModule.helloWorld());
+console.log('helloWorld module',helloWorldModule.helloWorld());
 
-// const additionModule = require('./additionModule.node');
-// console.log(additionModule.additionCalc(564984784894784787478748777878777877878797778,878787787778777));
+const additionModule = require('./additionModule.node');
+console.log('addition module',additionModule.additionCalc(75,20));
 
-// const multiplicationModule = require('./multiplicationModule.node');
-// console.log(multiplicationModule.multiplicationCalc(2,4));
-
-
-
-
-// const perlinModule = require('./perlinModule.node');
-// console.time('test');
-// console.log(perlinModule.generate(59.595,69.757));
-// console.timeEnd('test');
+const multiplicationModule = require('./multiplicationModule.node');
+console.log('multiplication module',multiplicationModule.multiplicationCalc(2,4));
